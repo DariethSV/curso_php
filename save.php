c57f478bc4a6e0c8ab6c2df8c55c523ef6e195c6
Darieth <darieth.s.v@gmail.com>
2025-07-22 09:14:27 -0500

    Add redirect to form after saving user data

--- save.php
+++ save.php
@@ -7,6 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($conexion->query($sql) === TRUE) {
         echo "Nombre guardado correctamente.";
+        header("Location: form.html");
+        exit();
+exit();
     } else {
         echo "Error: " . $conexion->error;
     }
