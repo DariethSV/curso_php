d0ea78131f127a627c6a491c4fe5d8684a6ec109
Darieth <darieth.s.v@gmail.com>
2025-07-22 09:10:34 -0500

    Separate form HTML from PHP logic

--- save.php
+++ save.php
@@ -13,17 +13,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registro de Nombre</title>
-</head>
-<body>
-    <h2>Ingresa tu nombre</h2>
-    <form method="POST" action="">
-        <input type="text" name="nombre" required>
-        <button type="submit">Guardar</button>
-    </form>
-</body>
-</html>
-
